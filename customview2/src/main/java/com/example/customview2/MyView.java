6bda747d2e768b4ce71869c74eb0ba7dba8bafe1
pooja2629 <pooja2629@users.noreply.github.com>
2018-01-02 15:12:21 +0530

    views2

---
+++ customview2/src/main/java/com/example/customview2/MyView.java
@@ -0,0 +1,42 @@
+package com.example.customview2;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.widget.TextView;
+
+/**
+ * Created by Pooja on 02-01-2018.
+ */
+
+public class MyView extends android.support.v7.widget.AppCompatTextView {
+     private Paint paint;
+
+     public  MyView (Context context){
+         super(context);
+         paint=new Paint();
+         paint.setStrokeWidth(3f);
+         paint.setColor(Color.RED);
+     }
+    public MyView(Context context, @Nullable AttributeSet attrs){
+        super(context, attrs);
+        paint=new Paint();
+        paint.setStrokeWidth(0.5f);
+        paint.setColor(Color.BLACK);
+    }
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+
+        //  canvas.drawCircle(100,50,100,paint);
+        paint.setColor(Color.BLACK);
+        for (int i=10;i<1000;i+=100){
+            canvas.drawLine(10,i,getWidth(),i,paint);
+        }
+        paint.setColor(Color.RED);
+        canvas.drawLine(200,0,200,getHeight(),paint);
+    }
+}
---
+++ customview3/src/main/java/com/example/customview3/MyView.java
@@ -0,0 +1,52 @@
+package com.example.customview3;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.widget.EditText;
+
+/**
+ * Created by Pooja on 02-01-2018.
+ */
+
+public class MyView extends EditText {
+
+    private Paint paint;
+
+    private float cx, cy;
+
+    public MyView(Context context) {
+        super(context);
+        init();
+    }
+
+    public MyView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init();
+    }
+    @Override
+    protected void onDraw(Canvas canvas) {
+
+        super.onDraw(canvas);
+        canvas.drawCircle(cx, cy, 30, paint);
+    }
+    @Override
+
+    public boolean onTouchEvent(MotionEvent event) {
+
+        cx = event.getX();
+        cy = event.getY();
+        invalidate();
+        return true;
+    }
+    private void init() {
+
+        paint = new Paint();
+        paint.setColor(Color.RED);
+        paint.setStrokeWidth(2);
+
+    }
+}

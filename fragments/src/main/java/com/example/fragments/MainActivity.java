4bde90343b388a256884909bf9c6c15a864848aa
pooja2629 <pooja2629@users.noreply.github.com>
2018-01-14 11:18:15 +0530

    fragments and remaining

--- fragments/src/main/java/com/example/fragments/MainActivity.java
+++ fragments/src/main/java/com/example/fragments/MainActivity.java
@@ -12,7 +12,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-       //loadFrangment(new BikeFragment());
+       loadFrangment(new BackgroundFragment());
     }
 
     public void loadFrangment(Fragment fragment){

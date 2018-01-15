3f178b755e774417b3b778a04cf58b6958a1ae12
pooja2629 <pooja2629@users.noreply.github.com>
2018-01-15 12:17:48 +0530

    dialogs

---
+++ dialogs/src/main/java/com/example/dialogs/AppDialog.java
@@ -0,0 +1,78 @@
+package com.example.dialogs;
+
+
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.app.ProgressDialog;
+import android.app.TimePickerDialog;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.RequiresApi;
+import android.support.v4.app.DialogFragment;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AlertDialog;
+import android.view.View;
+import android.widget.Toast;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class AppDialog extends DialogFragment {
+
+
+    public static final String TAG_ALERT="alert";
+    public static final String TAG_DATE="date";
+    public static final String TAG_TIME="time";
+    public static final String TAG_PROGRESS="progress";
+    public static final String TAG_CUSTOM="custom";
+
+
+
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        Dialog dialog = new Dialog(getActivity());
+
+        if (getTag().equals(AppDialog.TAG_ALERT)) dialog = alert();
+        else if (getTag().equals(AppDialog.TAG_DATE)) dialog = datePicker();
+        else if (getTag().equals(AppDialog.TAG_TIME)) dialog = timePicker();
+        else if (getTag().equals(AppDialog.TAG_PROGRESS)) dialog = progressDialog();
+        return dialog;
+    }
+    private AlertDialog alert(){
+        AlertDialog.Builder builder=new AlertDialog.Builder(getActivity());
+        builder.setTitle(getResources().getString(R.string.title))
+                .setIcon(R.mipmap.ic_launcher)
+                .setMessage(getResources().getString(R.string.msg))
+                .setPositiveButton(R.string.btnYes,(dialog, which) -> mt("YES"))
+                .setNegativeButton(R.string.btnNO,(dialog, which) -> mt("NO"));
+        return builder.create();
+
+    }
+
+
+    private DatePickerDialog datePicker(){
+
+        return new DatePickerDialog(getActivity(),(view,year, month, dayOfMonth)->
+                mt(""+dayOfMonth +"-"+ (month+1) +"-"+ year),2018,0,15);
+    }
+
+    private TimePickerDialog timePicker(){
+        return  new TimePickerDialog(getActivity(),(view, hourOfDay, minute)->mt
+                (""+hourOfDay+":"+minute),12,5,false);
+    }
+    private ProgressDialog progressDialog(){
+        ProgressDialog dialog= new ProgressDialog(getActivity());
+        dialog.setTitle(getString(R.string.title));
+        dialog.setMessage(getString(R.string.msg));
+       // dialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+        dialog.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
+         return dialog;
+    }
+
+    private void mt(String msg){
+        Toast.makeText(getActivity(),msg, Toast.LENGTH_SHORT).show();
+    }
+}
